e742457f9e78784f6b98bc3e660ffdf0eb735909
Sameer Kumar <sameerkr.dev@gmail.com>
2025-12-01 16:53:23 +0530

    add: sidebar and dashboard layout

--- src/layouts/AuthorizedLayout.tsx
+++ src/layouts/AuthorizedLayout.tsx
@@ -1,9 +1,39 @@
 import { Outlet } from "react-router";
 
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+
 const AuthorizedLayout = () => {
   return (
     <div>
-      <Outlet />
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <SiteHeader />
+          <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+              <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                {/* <SectionCards />
+                <div className="px-4 lg:px-6">
+                  <ChartAreaInteractive />
+                </div> */}
+                {/* <DataTable data={data} /> */}
+                <div className="px-4 lg:px-6">
+                  <Outlet />
+                </div>
+              </div>
+            </div>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
     </div>
   );
 };
